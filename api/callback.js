1db01c2ddc3bc2bac10e5d44b4405dbc9e3f8699
Jeremy CAMP <jry_camp@proton.me>
2025-06-23 20:10:30 +0200

    Handle code query parameter in callback API route

--- api/callback.js
+++ api/callback.js
@@ -1,16 +1,9 @@
 export default function handler(req, res) {
-  const { code, state, error } = req.query;
-
-  if (error) {
-    return res.status(400).json({ error: error });
-  }
+  const { code } = req.query;
 
   if (!code) {
-    return res.status(400).json({ error: "No code provided" });
+    return res.status(400).json({ error: 'No code provided' });
   }
 
-  console.log("OAuth code reçu :", code);
-  console.log("State :", state);
-
-  res.status(200).json({ message: "Code reçu", code, state });
+  res.status(200).json({ code });
 }
