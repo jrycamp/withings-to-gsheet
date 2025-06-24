ae3dbb47d01669e5ac0c3a3c8117f27a02d68dc9
Jeremy CAMP <jry_camp@proton.me>
2025-06-24 13:36:08 +0200

    Recupération réponse Withings

--- api/callback.js
+++ api/callback.js
@@ -8,6 +8,7 @@ export default async function handler(req, res) {
   const tokenUrl = 'https://account.withings.com/oauth2/token';
 
   const params = new URLSearchParams({
+    action: 'requesttoken',
     grant_type: 'authorization_code',
     client_id: process.env.CLIENT_ID,
     client_secret: process.env.CLIENT_SECRET,
@@ -25,9 +26,15 @@ export default async function handler(req, res) {
     });
 
     const rawText = await response.text();
-    console.log('RAW RESPONSE FROM WITHINGS:', rawText);
 
-    res.status(response.status).send(`<pre>${rawText}</pre>`);
+    // Affichage plus clair
+    res.setHeader('Content-Type', 'text/html');
+    res.status(response.status).send(`
+      <h2>Requête envoyée à Withings</h2>
+      <pre>${params.toString()}</pre>
+      <h2>Réponse reçue :</h2>
+      <pre>${rawText}</pre>
+    `);
   } catch (err) {
     console.error('Error fetching token:', err);
     res.status(500).send('Server error during token exchange');
